20c604ba45137de284af922d8aca4e70def759eb
Celestin Joy <138105920+Celestinjoy@users.noreply.github.com>
2023-11-30 15:14:53 +0530

    Create linearsearch.java

---
+++ linearsearch.java
@@ -0,0 +1,7 @@
+public static int linearSearch(int[] array, int number) {
+        for (int i =0; i < array.length;i++) {
+            if (array[i] == number)
+               return i;
+        }
+        return -1;
+    }
