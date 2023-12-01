0751374ef9ccbd46e591db6fef4746073ae40453
Celestin Joy <138105920+Celestinjoy@users.noreply.github.com>
2023-12-01 08:30:24 +0530

    Create mergeSort.java

---
+++ mergeSort.java
@@ -0,0 +1,63 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 5, 3, 6, 8, 3, 8};
+
+        mergeSort(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+    public static void mergeSort(int[] array){
+        int length = array.length;
+        if (length <= 1) return;
+
+        int middle = length/2;
+        int[] leftArray = new int[middle];
+        int[] rightArray = new int[length - middle];
+
+        int i = 0;
+        int j = 0;
+        for (;i < length;i++) {
+            if (i < middle)
+                leftArray[i] = array[i];
+            else {
+                rightArray[j] = array[i];
+                j++;
+            }
+        }
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+        merge(leftArray,rightArray,array);
+    }
+
+    public static void merge(int[] leftArray,int[] rightArray,int[] array) {
+    int leftsize = array.length/2;
+    int rightsize = array.length - leftsize;
+    int i =0;
+    int l =0;
+    int r =0;
+
+    while (l < leftsize && r < rightsize) {
+        if (leftArray[l] < rightArray[r]) {
+            array[i] = leftArray[l];
+            i++;
+            l++;
+        }
+        else {
+            array[i] = rightArray[r];
+            i++;
+            r++;
+        }
+    }
+    while (l < leftsize) {
+        array[i] = leftArray[l];
+        i++;
+        l++;
+    }
+    while (r < rightsize) {
+        array[i] = rightArray[r];
+        i++;
+        r++;
+    }
+    }
+}
